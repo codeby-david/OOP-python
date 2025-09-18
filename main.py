488640fca238b92be8c9c9e01c00e4362d7cba78
DavidM <169084424+codeby-david@users.noreply.github.com>
2025-09-18 12:07:49 +0300

    added some examples

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+'''
+Four pillars of OOP?
+
+1.Encapsulation → Wrapping data & methods together.
+
+2.Abstraction → Hiding implementation details, showing only necessary features.
+
+3.Inheritance → One class inherits properties of another.
+
+4.Polymorphism → One name, many forms (method overriding/overloading).
+'''
+#Classes and objects in python
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+my_car = Car("Toyota", "Corolla")  # Object
+print(my_car.brand)  # Toyota
+
+#__init__ in Python-A special constructor method that runs when an object is created.Used to initialize object properties.
+
+#Example of Inheritance
+class Animal:
+    def speak(self):
+        print("Animal speaks")
+
+class Dog(Animal):  # Inheritance
+    def speak(self):
+        print("Dog barks")
+
+dog = Dog()
+dog.speak()  # Dog barks
+
 
